c555a4236c8fa02843ce25a9795ce351377abb02
kasty <kastytis.slubelis@gmail.com>
2020-07-21 18:50:35 +0300

    Using Redux

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,79 +1,56 @@
 import React, { Component } from 'react';
-import { ScrollView, View, Text } from 'react-native';
+import { ScrollView, View, FlatList, Text } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
-import { FlatList } from 'react-native-gesture-handler';
-import { LEADERS } from '../shared/leaders';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+const mapStateToProps = state => {
+    return {
+        leaders: state.leaders
+    }
+}
 
 function History() {
 
     return(
         <Card title="Our History">
             <Text style={{ margin: 10 }}>
-            Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. 
-            With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the 
-            A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never 
-            know what will arrive on your plate the next time you visit us.
-            {"\n\n"}
-            The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first
-            time the world's best cuisines in a pan.
+                Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that 
+                can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never 
+                know what will arrive on your plate the next time you visit us.
+                {"\n\n"}
+                The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time 
+                the world's best cuisines in a pan.
             </Text>   
         </Card>
     );
 };
 
-function CorporateLeadership(props) {
-    const leaders = props.leaders;
-
-    if (leaders  != null) {
-
-        const renderLeader = ({item, index}) => {
-            return(
-                <ListItem 
-                 key={index}
-                 title={item.name} 
-                 subtitle={item.description}
-                 hideChevron={true}
-                 leftAvatar={{ source: require('./images/alberto.png')}} />
-            );
-        };
-
-        return(
-            <Card title='Corporate Leadership'>
-                <FlatList
-                    data={leaders}
-                    renderItem={renderLeader}
-                    keyExtractor={item => item.id.toString()}
-                    />                
-            </Card>
-        );
-     } 
-
-    else {
-        return(<View></View>)
-    }
-}
-
-class About extends Component{
-
-    constructor(props){
-        super(props);
-        this.state = {
-            leaders: LEADERS
-        };
-    }
+class About extends Component {
 
     static navigationOptions = {
         title: 'About Us'
     };
 
     render() {
-        return(
-                <ScrollView>
-                    <History />
-                    <CorporateLeadership leaders={this.state.leaders}/>
-                </ScrollView>
+        const { params } = this.props.navigation.state;
+        const renderLeader = ({item, index}) => {
+            return (
+                <ListItem roundAvatar key={index} title={item.name} subtitle={item.description} subtitleNumberOfLines={15}
+                hideChevron={true} leftAvatar={{ source: { uri: baseUrl + item.image}}} />
+            );
+        };
+
+        return (
+            <ScrollView>
+                <History/>
+                <Card title="Corporate Leadership">
+                <FlatList data={this.props.leaders.leaders} renderItem={renderLeader} keyExtractor={item => item.id.toString()}
+                    />
+                </Card> 
+            </ScrollView>            
         );
     };
-}
+};
 
-export default About;
+export default connect(mapStateToProps)(About);
